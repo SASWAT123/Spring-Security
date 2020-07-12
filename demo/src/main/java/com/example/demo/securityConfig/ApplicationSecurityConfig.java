b02de63898d5cdb4f12c9178aa5b951fbc4ba611
SASWAT123 <psaswat21@gmail.com>
2020-07-12 15:19:21 +0530

    Permission based authentication using antmatchers

--- demo/src/main/java/com/example/demo/securityConfig/ApplicationSecurityConfig.java
+++ demo/src/main/java/com/example/demo/securityConfig/ApplicationSecurityConfig.java
@@ -3,6 +3,7 @@ package com.example.demo.securityConfig;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -23,6 +24,9 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
 		http.csrf().disable()
 			.authorizeRequests()
 			.antMatchers("/api/**").hasRole(ApplicationUserRole.STUDENT.name())	//Protects the api only for STUDENT roles
+			.antMatchers(HttpMethod.DELETE,"/management/api/**").hasAuthority(ApplicationUserPermission.COURSE_WRITE.getPermission())
+			.antMatchers(HttpMethod.POST,"/management/api/**").hasAuthority(ApplicationUserPermission.COURSE_WRITE.getPermission())
+			.antMatchers(HttpMethod.GET,"/management/api/**").hasAnyRole(ApplicationUserRole.ADMIN.name(), ApplicationUserRole.ADMINTRAINEE.name())
 			.anyRequest()
 			.authenticated()
 			.and()
@@ -35,19 +39,22 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
 		UserDetails saswatUser = User.builder()
 				.username("saswat")
 				.password(passwordEncoderConfig.passwordEncoder().encode("password")) //Encrypting password using BCrypt encoder
-				.roles(ApplicationUserRole.STUDENT.name()) //ROLE_STUDENT
+//				.roles(ApplicationUserRole.STUDENT.name()) //ROLE_STUDENT
+				.authorities(ApplicationUserRole.STUDENT.getGrantedAuthorities())
 				.build();
 		
 		UserDetails adminUser = User.builder()
 				.username("admin")
 				.password(passwordEncoderConfig.passwordEncoder().encode("password123")) //Encrypting password using BCrypt encoder
-				.roles(ApplicationUserRole.ADMIN.name()) //ROLE_ADMIN
+//				.roles(ApplicationUserRole.ADMIN.name()) //ROLE_ADMIN
+				.authorities(ApplicationUserRole.ADMIN.getGrantedAuthorities())
 				.build();
 		
 		UserDetails adminTraineeUser = User.builder()
 				.username("admintrainee")
 				.password(passwordEncoderConfig.passwordEncoder().encode("password123")) //Encrypting password using BCrypt encoder
-				.roles(ApplicationUserRole.ADMINTRAINEE.name()) //ROLE_ADMINTRAINEE
+//				.roles(ApplicationUserRole.ADMINTRAINEE.name()) //ROLE_ADMINTRAINEE
+				.authorities(ApplicationUserRole.ADMINTRAINEE.getGrantedAuthorities())
 				.build();
 		
 		return new InMemoryUserDetailsManager(saswatUser, adminUser, adminTraineeUser);
--- demo/src/main/java/com/example/demo/securityConfig/ApplicationUserRole.java
+++ demo/src/main/java/com/example/demo/securityConfig/ApplicationUserRole.java
@@ -1,6 +1,10 @@
 package com.example.demo.securityConfig;
 
 import java.util.Set;
+import java.util.stream.Collectors;
+
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+
 import com.google.common.collect.Sets;
 
 public enum ApplicationUserRole {
@@ -14,6 +18,19 @@ public enum ApplicationUserRole {
 
 	ApplicationUserRole(Set<ApplicationUserPermission> permissions) {
 		this.permissions = permissions;
+	}
+	
+	public Set<ApplicationUserPermission> getPermissions(){
+		return permissions;
+	}
+	
+	public Set<SimpleGrantedAuthority> getGrantedAuthorities(){
+		Set<SimpleGrantedAuthority> permissions = getPermissions().stream()
+				.map(permission -> new SimpleGrantedAuthority(permission.getPermission()))
+				.collect(Collectors.toSet());
+		
+		permissions.add(new SimpleGrantedAuthority("ROLE_" + this.name()));
+		return permissions;
 	}
 
 }
